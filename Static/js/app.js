965bb2e9a15555b240fd0d23222c6e9fd24d1a95
Charlesopuba <charles.opuba@student.moringaschool.com>
2021-06-13 09:42:14 +0300

    added b1 image,styled form

---
+++ Static/js/app.js
@@ -0,0 +1,5 @@
+//This part you enables the whole magic to happen//
+var names = function(a,b,c,d){
+    var malenames = ['Kwasi','Kudwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+}
