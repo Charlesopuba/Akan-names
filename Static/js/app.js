ad5acd1426b84b4cccc60b588a0261668bf0bdcd
Charlesopuba <charles.opuba@student.moringaschool.com>
2021-06-13 09:53:42 +0300

    js part

--- Static/js/app.js
+++ Static/js/app.js
@@ -3,3 +3,25 @@ var names = function(a,b,c,d){
     var malenames = ['Kwasi','Kudwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 }
+var d = new Date(y, --m, d);
+if (g === "Female") {
+    return d && femaleNames[d.getDay()];
+} else {
+    return d && maleNames[d.getDay()];
+}
+
+
+// User interface (or front-end) logic:
+$(document).ready(function() {
+$("form#form").submit(function(event) {
+    event.preventDefault();
+    var y = parseInt($("#year").val());
+    var m = parseInt($("#month").val());
+    var d = parseInt($("#date").val());
+    var g = $("input:radio[name=gender]:checked").val();
+    var result = akan(y, m, d, g);
+    alert("Your akan name is: " + result);
+    //refresh page
+    document.getElementById("form").reset();
+});
+});
